3cc0a58145d88d5d0b2fd380b687d8055afe2cdb
Marta_kachanova <marta.kachanova@gmail.com>
2022-10-21 01:19:45 +0300

    projec

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,21 +3,21 @@ import { useState } from "react";
 
 function Questions({ items, buttons }) {
 
-let [colorBg, setColor] = useState({buttons});
+// let [colorBg, setColor] = useState({buttons});
 
-const buttonColor = (e) => {
-if(e.target.id === "btn_1"){
-  setColor({buttons: "FFE4B5#"});
-}
-if(e.target.id === "btn_2"){
-  setColor({buttons: "#00FA9A"});
-}
-if(e.target.id === "btn_3"){
-  setColor({buttons: "#FFA07A"});
-}
-return colorBg;
-}
-const style = { backgroundColor: colorBg.buttons};
+// const buttonColor = (e) => {
+// if(e.target.id === "btn_1"){
+//   setColor({buttons: "FFE4B5#"});
+// }
+// if(e.target.id === "btn_2"){
+//   setColor({buttons: "#00FA9A"});
+// }
+// if(e.target.id === "btn_3"){
+//   setColor({buttons: "#FFA07A"});
+// }
+// return colorBg;
+// }
+// const style = { backgroundColor: colorBg.buttons};
 
 
 return (
@@ -31,8 +31,9 @@ return (
     
     <div className="status" >
       {buttons.map((item) => (
-          <button className="dropbtn" onClick={buttonColor} style={style}>
-            <a href="#">{item.value} </a>
+          <button className="dropbtn" 
+          // onClick={buttonColor} style={style}
+          > <a href="#">{item.value} </a>
           </button>
         ))} 
     </div>
